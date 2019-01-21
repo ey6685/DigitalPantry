5cd5d72bc50bb2a0657c5d4e3cac83a1e30978e8
Jon <ey6685@wayne.edu>
2019-01-21 15:24:40 -0500

    Finalize add-ingredient page v1
    -Removed MongoDB functions and files that could cause conflicts on development
    -Kept config/database.js file since that will be needed eventually
    -add_ingredient.pug is basic form for creating new ingredient
    -Commented out add-ingredient post function in app.js to avoid potential conflicts on dev.
    -models/ingredient.js created for sequelize connection.

--- app.js
+++ app.js
@@ -3,53 +3,29 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
-
-// //Need to move to connect route
-// mongoose.connect('mongodb://localhost/local');
-// let db = mongoose.connection;
-
-// //Need to move to connect route
-// //Check connection
-// db.once('open', function(){
-//     console.log('Connected to MongoDB');
-// });
-
-// //Need to move to connect route
-// //Check for MongoDB errors
-// db.on('error', function(err){
-//     console.log(err);
-// });
 
 //Initialize the application
 
-const Sequelize = require('sequelize');
-const db = new Sequelize('local', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false,
+// const Sequelize = require('sequelize');
+// const db = new Sequelize('local', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     operatorsAliases: false,
     
-    pool:{
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-});
+//     pool:{
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// });
 
-//created /config/database.js for connection module
-//const db = require('./config/database');
-
-//Test DB
-db.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('Error: ' + err))
+// //Test DB
+// db.authenticate()
+//     .then(() => console.log('Database connected...'))
+//     .catch(err => console.log('Error: ' + err))
 
 const app = express();
-
-
-//Bring in Models
-//let Ingredient = require('./models/ingredient');
 
 //TODO setup database connections somewhere here
 
@@ -74,41 +50,32 @@ app.get('/', function(req, res){
     });
 });
 
-//added Route for ingredient
-//app.use('/ingredients', require('./routes/ingredients'));
+// //Ingredient Add Route
+// app.get('/ingredients/add', function(req,res){
+//     res.render('add_ingredient',{
+//         title:'Add Ingredient'
+//     });
+// });
 
-//create connection on PORT 3000 for mysql
-//const PORT = process.env.PORT || 3000;
+// //Add Submit POST Route
+// app.post('/ingredients/add', function(req,res){
+//     let ingredient = new ingredient();
+//     console.log("testing" + req.body.name);
 
-//start listening on PORT 3000
-//app.listen(PORT, console.log(`Server started on port ${PORT}`));
+//     ingredient.name = req.body.name;
+//     ingredient.measurement = req.body.measurement;
+//     ingredient.servingsize = req.body.servingsize;
+//     ingredient.expiration = req.body.expiration;
 
-//Ingredient Add Route
-app.get('/ingredients/add', function(req,res){
-    res.render('add_ingredient',{
-        title:'Add Ingredient'
-    });
-});
-
-//Add Submit POST Route
-app.post('/ingredients/add', function(req,res){
-    let ingredient = new ingredient();
-    console.log("testing" + req.body.name);
-
-    ingredient.name = req.body.name;
-    ingredient.measurement = req.body.measurement;
-    ingredient.servingsize = req.body.servingsize;
-    ingredient.expiration = req.body.expiration;
-
-    ingredient.save(function(err){
-        if(err){
-            console.log(err);
-            return;
-        } else{
-            res.redirect('/');
-        }
-    });
-});
+//     ingredient.save(function(err){
+//         if(err){
+//             console.log(err);
+//             return;
+//         } else{
+//             res.redirect('/');
+//         }
+//     });
+// });
 
 //Route files
 const users = require('./routes/users');
