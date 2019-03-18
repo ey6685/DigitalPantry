8bddd837decfd613023a72fefbd1ad2bfd06dd2d
Jon <ey6685@wayne.edu>
2019-03-17 21:29:33 -0400

    Ingredients Pages Fixed
    -added post route for showall and get route for expired ingredients back in from development
    -updated showall/add to use new DB tables and variable names
    -converted showall/add to use Sequelize method instead of raw SQL
    -All ingredients pages working now

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -8,13 +8,13 @@ const aw = require('../algorithm/auto_weight')
 // Render page with data from database
 // GET request to localhost:3000/users/login
 router.get('/showall', function(req, res) {
-  //renders showall_recipes with all the available ingredients
+  // renders showall_recipes with all the available ingredients
 
   ////////////////////////////////
-  //need to add pantry id feching//
+  // need to add pantry id feching//
   /////////////////////////////////
   db.query(
-    'select ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null;',
+    'select ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date >= CURDATE();',
     function(err, results) {
       for (key in results) {
         results[key]['ingredient_expiration_date'] = moment(
@@ -23,6 +23,75 @@ router.get('/showall', function(req, res) {
       }
       if (err) throw err
       res.render('showall_ingredients', {
+        title: 'Your Ingredients',
+        results: results
+      })
+    }
+  )
+})
+
+// POST request to localhost:3000/ingredients/add
+// This will add a new ingredient to available ingredients and update database
+// Created by Jon
+// Last modified: 03/17/2019
+// Modified by: Jon
+// Modification:
+//  Updated route to use new DB tables and variables names
+//  Updated route to use Sequelize instead of raw SQL
+router.post('/showall', async function(req, res) {
+  // get parameters from request body
+  console.log(req.body)
+
+  var ingredient_name = req.body.ingredient_name
+  var ingredient_amount = req.body.ingredient_total
+  var ingredient_unit = req.body.ingredient_measurement
+  var ingredient_date = req.body.ingredient_expiration_date
+  var final_id
+
+  var does_ingredient_exist = await ingredient_t.findOne({
+    where: {
+      ingredient_name: ingredient_name
+    }
+  })
+
+  if (does_ingredient_exist != null) {
+    final_id = does_ingredient_exist.ingredient_id
+  } else {
+    var new_weight = await aw.auto_weight(ingredient_name)
+    var new_ingredient = await ingredient_t.create({
+      ingredient_name: ingredient_name,
+      ingredient_weight: new_weight
+    })
+    final_id = new_ingredient.ingredient_id
+  }
+
+  var new_inv = ing_in_stock.create({
+    pantry_id: 1, //change to get this from the sesson
+    ingredient_id: final_id,
+    ingredient_amount: ingredient_amount,
+    ingredient_unit_of_measurement: ingredient_unit,
+    ingredient_expiration_date: ingredient_date
+  })
+
+  console.log('ingredient add: \n' + JSON.stringify(new_inv))
+
+  res.redirect('/ingredients/showall')
+})
+
+// Render page with data from database
+// GET request to localhost:3000/users/login
+router.get('/expired', function(req, res) {
+  // renders showall_recipes with all the available ingredients
+  db.query(
+    'select ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date < CURDATE();',
+    function(err, results) {
+      for (key in results) {
+        results[key]['ingredient_expiration_date'] = moment(
+          results[key]['ingredient_expiration_date']
+        ).format('LL')
+      }
+      if (err) throw err
+      res.render('expired_ingredients_np', {
         title: 'Your Ingredients',
         results: results
       })
@@ -41,7 +110,7 @@ router.get('/add', function(req, res) {
 // Render page with data from database
 // GET request to localhost:3000/users/login
 router.get('/expiredAdmin', function expiredTable(req, res) {
-  //renders showall_recipes with all the available ingredients
+  // renders showall_recipes with all the available ingredients
   db.query(
     'select ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date < CURDATE();',
     function(err, results) {
@@ -102,8 +171,9 @@ router.post('/add', async function(req, res) {
   }
   res.redirect('/ingredients/showall')
 })
-//Render page with data from database
-//GET request to localhost:3000/ingredients/cards
+
+// Render page with data from database
+// GET request to localhost:3000/ingredients/cards
 router.get('/cards', function showCards(req, res) {
   const query =
     'select ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null;'
@@ -121,7 +191,7 @@ router.get('/cards', function showCards(req, res) {
   })
 })
 
-//remove ingredient by id
+// remove ingredient by id
 router.delete('/remove/', async function(req, res) {
   const ingredient = req.body.id
   var ex_date = req.body.ex
