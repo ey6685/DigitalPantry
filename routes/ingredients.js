2487bfb87a0bca10c9c704ecb82fbc2bf9e559c0
Jon <ey6685@wayne.edu>
2019-03-28 01:39:42 -0400

    Thumbnails Ingredients Table
    -Thumbnails now working in Ingredients table
    -Updated get route for ingredients/showall to grab ingredient_image_path
    -Revised nav-bar to include pantry thumbnail

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -11,14 +11,14 @@ const User = require('../DB_models/Users')
 //defines where to store image
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, __dirname + '/../public/images/');
+    cb(null, __dirname + '/../public/images/')
   },
   filename: function(req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname)
   }
-});
+})
 // create an upload function using configuration above
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 
 // Render page with data from database
 // GET request to localhost:3000/users/login
@@ -29,7 +29,7 @@ router.get('/showall', function(req, res) {
   // need to add pantry id feching//
   /////////////////////////////////
   db.query(
-    'select ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date >= CURDATE();',
+    'select ingredients.ingredient_image_path, ingredients.ingredient_id, ingredients.ingredient_name, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date >= CURDATE();',
     function(err, results) {
       for (key in results) {
         results[key]['ingredient_expiration_date'] = moment(
@@ -146,7 +146,6 @@ router.get('/expiredAdmin', function expiredTable(req, res) {
 // POST request to localhost:3000/ingredients/add
 // This will add a new ingredient to available ingredients and update database
 router.post('/add', upload.single('image'), async function addIngredient(req, res) {
-
   if (req.file) {
     var imagePath = req.file.filename
     console.log('File Uploaded Successfully')
@@ -162,16 +161,14 @@ router.post('/add', upload.single('image'), async function addIngredient(req, re
     console.log('File Upload Failed')
   }
 
-
-
   for (var key in req.body) {
     if (key.includes('ingredientProperties')) {
       var block_oF_data = req.body[key]
       console.log(block_oF_data)
-      console.log('0 is '+block_oF_data[0])
-      console.log('1 is '+block_oF_data[1])
-      console.log('2 is '+block_oF_data[2])
-      console.log('3 is '+block_oF_data[3])
+      console.log('0 is ' + block_oF_data[0])
+      console.log('1 is ' + block_oF_data[1])
+      console.log('2 is ' + block_oF_data[2])
+      console.log('3 is ' + block_oF_data[3])
       //seperate the data
       var ingredient_name = block_oF_data[0]
       var ingredient_amount = block_oF_data[1]
@@ -226,13 +223,9 @@ router.get('/cards', function showCards(req, res) {
     res.render('showall_ingredients_cards', {
       title: 'Your Ingredients',
       results: results
-
-    
     })
   })
 })
-
-
 
 // remove ingredient by id
 router.delete('/remove/', async function remove(req, res) {
