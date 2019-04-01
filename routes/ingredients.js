7bc565dcffaad870c561fa9640bc9a263965974c
patrick veltri <et8492@wayne.edu>
2019-04-01 01:56:45 -0400

    fixed ingredient finding

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -38,7 +38,7 @@ router.get('/showall', async function(req, res) {
   // need to add pantry id feching//
   /////////////////////////////////
   db.query(
-    'select ingredients.ingredient_image_path, ingredients.ingredient_id, ingredients.ingredient_name, ingredients.priority, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date >= CURDATE();',
+    'select ingredients.ingredient_image_path, ingredients.ingredient_id, ingredients.ingredient_name, ingredients.priority, ingredients_in_pantry.ingredient_amount, ingredients_in_pantry.ingredient_unit_of_measurement, ingredients_in_pantry.ingredient_expiration_date from ingredients_in_pantry inner join ingredients on ingredients_in_pantry.ingredient_id = ingredients.ingredient_id and ingredients_in_pantry.ingredient_expiration_date is not null AND ingredient_expiration_date >= CURDATE() and pantry_id=' + currentPantryId.pantry_id+';',
     function(err, results) {
       for (key in results) {
         results[key]['ingredient_expiration_date'] = moment(
