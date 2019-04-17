52fa9ccd994d16ac7bcecbbc4cf33ab3e1920b00
Oskars Dauksts <dauksts.oskars@gmail.com>
2019-04-17 01:35:00 -0400

    merge conflicts fix

--- routes/recipes.js
+++ routes/recipes.js
@@ -262,7 +262,7 @@ router.post('/add', upload.single('image'), async function addRecipe(req, res) {
   // recipe_name and recipe_size are unique form fields, so they do not require any recursion to grab all of them
   //const recipeName = input_cleaner.string_cleaning(req.body.recipeName)
   const recipeServingSize = (req.body.recipeServingSize)
-  var recipeDirections = (req.body.recipeDirections)
+  const recipeDirections = (req.body.recipeDirections)
   const recipeShareable = req.body.recipeShareable
   let replaceNewLine = '#'
   for (char in recipeDirections) {
@@ -270,8 +270,6 @@ router.post('/add', upload.single('image'), async function addRecipe(req, res) {
       recipeDirections[char].replace('\r', '').replace('\n', '#')
     )
   }
-  //gets rid oof the first #
-  replaceNewLine = replaceNewLine.substring(1);
   // INSERT new recipe information into table
   const result = await recipe_t
     .create({
@@ -299,7 +297,7 @@ router.post('/add', upload.single('image'), async function addRecipe(req, res) {
         var ingredient_data_from_page = req.body.ingredientProperties[i]
 
         const ingredientName = await input_cleaner.string_cleaning(ingredient_data_from_page[0])
-        const ingredientQuantity = await input_cleaner.num(ingredient_data_from_page[1])
+        const ingredientQuantity =ingredient_data_from_page[1]
         const ingredientMeasurement = ingredient_data_from_page[2]
         console.log("adding ingredient to recipe: \n")// + ingredientQuantity+ " " +ingredientMeasurement+" of " + ingredientName);
         console.log("ingredientName: " + ingredientName);
@@ -490,9 +488,6 @@ router.post('/edit', async function editRecipe(req, res) {
   const recipeName = await input_cleaner.string_cleaning(req.body.recipeName)
   const recipeSize = await input_cleaner.string_cleaning(req.body.recipeSize)
   const recipeId = await input_cleaner.string_cleaning(req.body.recipeId)
-  const newIngredientNames = (req.body.ingredientName)
-  const newIngredientQtys = await input_cleaner.string_cleaning(req.body.ingredientQty)
-  const newIngredientMeasurements = await input_cleaner.string_cleaning(req.body.ingredientMeasurement)
   const recipeDirections = await input_cleaner.string_cleaning(req.body.recipeDirections)
   const ingredientNamesArray = req.body.ingredientName
   const ingredientQtyArray = req.body.IngredientQty
@@ -562,7 +557,7 @@ router.post('/edit', async function editRecipe(req, res) {
     }
     })
   }
-  //Check if multiple ingredients have been added
+  //Check if multiple ingredients ahve ben
   if (newIngredientNameArray.constructor === Array){
     //Add new ingredients which are added to the recipe which is being edited
     for (newIngredient in newIngredientNameArray){
