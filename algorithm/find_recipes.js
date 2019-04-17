4549b9731fe0a91f76a5ac2f3c6af9c46f1f1772
patrick veltri <et8492@wayne.edu>
2019-04-17 15:56:38 -0400

    dead code removal

--- algorithm/find_recipes.js
+++ algorithm/find_recipes.js
@@ -19,123 +19,6 @@ const ing_totaler = require('./total_of_ingredient');
 const ingredient_t = require('../DB_models/Ingredients');
 const direction_parser = require('../recipe_direction_parser');
 const JSON_sort = require('sort-json-array');
-async function find_recipes(exp_id,pantry_id)
-{
-    try{
-        var recommened_recipes= new Array();
-        logger.info("starting find recipe with ingredient id: " + exp_id+"\n");
-        if(pantry_id == null) pantry_id = 1;
-        if(exp_id != null)
-        {
-            //find all the variant recipe ids that have the ingredieint
-            var recipe_ids = await ingredients_in_a_recipe_t.findAll({
-                attributes :['recipe_id'],
-                where:{
-                    ingredient_id: exp_id,
-                    pantry_id: pantry_id
-                }
-            });
-
-            logger.info('\nrecipe ids: ' +JSON.stringify(recipe_ids));
-
-            var recipe_ids_array = new Array();
-
-            recipe_ids.forEach(ids => {
-
-                recipe_ids_array.push(ids.recipe_id);
-            });
-            logger.info("array of recipes : \n" + recipe_ids_array);
-
-            //new vars for for loops
-            var flag_have_needed_anmount = false//starts false will flip if we have the ingredents
-            var ingredents_from_pantry_data;//stores the db called data
-            var list_of_ingredients_in_recipe;//for what is needed in the recipes
-            var i = 0//counter for the loop
-            var total_amount_ingredient=0//for totaling if there are mult instances of the ingredient
-         
-            for(i; i<recipe_ids_array.length;i++)
-            {
-                //set start data
-                total_amount_ingredient = 0;
-                flag_have_needed_anmount = true;
-                logger.info("\nrecipe_id: " + JSON.stringify(recipe_ids[i] ));
-                //get a list of ingredients need for 
-                list_of_ingredients_in_recipe = await ingredients_in_a_recipe_t.findAll({
-                    attributes: ['ingredient_id','amount_of_ingredient_needed', 'ingredient_unit_of_measurement'],
-                    where:{
-                        recipe_id: recipe_ids_array[i],
-                        pantry_id: pantry_id
-                    }
-                });
-                logger.info(JSON.stringify(list_of_ingredients_in_recipe));
-                //check each needed amount of the recipe and change the flag if we doent
-                for(var o=0; o< list_of_ingredients_in_recipe.length; o++)
-                {
-                    ingredents_from_pantry_data = await ingredients_in_pantry_t.findAll({
-                        attributes: ['ingredient_amount','ingredient_unit_of_measurement'],
-                        where: {
-                            ingredient_id: list_of_ingredients_in_recipe[o].ingredient_id,
-                            pantry_id: pantry_id
-                        }
-                    })
-
-                    total_amount_ingredient = 0;
-                    logger.info(JSON.stringify(ingredents_from_pantry_data));
-                    for(var p=0; p<ingredents_from_pantry_data.length; p++)
-                    {
-                        logger.info("list_of_ingredients_in_recipe[o].ingredient_unit_of_measurement: " + list_of_ingredients_in_recipe[o].ingredient_unit_of_measurement + ' == ' + ingredents_from_pantry_data[p].ingredient_unit_of_measurement);
-                        if(list_of_ingredients_in_recipe[o].ingredient_unit_of_measurement == ingredents_from_pantry_data[p].ingredient_unit_of_measurement)
-                        {
-                            total_amount_ingredient += ingredents_from_pantry_data[p].ingredient_amount;
-                        }
-                        else
-                        {
-                            var new_amount;
-                            logger.info("have to convert units");
-                            new_amount = await parseFloat(unit_convert.converter_raw(ingredents_from_pantry_data[p].ingredient_amount, ingredents_from_pantry_data[p].ingredient_unit_of_measurement,list_of_ingredients_in_recipe[o].ingredient_unit_of_measurement));
-                            logger.info("converted amount: " +new_amount);
-                            if(new_amount != 0)
-                            {
-                                total_amount_ingredient += new_amount;
-                            }   
-                        }
-                        logger.info("\ntotal: " + total_amount_ingredient + "\n");
-                    }//end of double nesst for loop
-                    // logger.info("total_amount_ingredient: " + total_amount_ingredient + " < " +list_of_ingredients_in_recipe[o].amount_of_ingredient_needed);
-                    if(total_amount_ingredient < list_of_ingredients_in_recipe[o].amount_of_ingredient_needed)
-                    {
-                        logger.info("cant cook, next\n");
-                        flag_have_needed_anmount =false;
-                        break;
-                    }
-
-
-                }//end of nested for loop
-                if(flag_have_needed_anmount)
-                    {
-                        recommened_recipes.push(recipe_ids_array[i]);
-                    }
-                logger.info("\n");
-            }//END OF FOR LOOP
-            
-            logger.info("recommended recipe ids: \n" + (recommened_recipes));
-            return recommened_recipes;
-        }//end of if checking that we passed an id for ingfredients
-        else{
-            logger.info("need ingredient id thank you");
-        }
-    }
-    catch(err)
-    {
-        logger.info(err);
-    }
-
-}
-
-module.exports.find_recipes = find_recipes;
-// find_recipes(1,1);
-
-
 /*
 function: find_recipes_no_inv
 
@@ -160,7 +43,7 @@ returns
 
 description
 checks for the best recipes for the ingredient id provided and returns them
-*/
+does not check if we have all the ingredientis*/
 
 async function find_recipes_no_inv(ingredient_id, pantry_id,scale){
     
